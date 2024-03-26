6e0d242d893642a14a08c13d4f92449de1849905
Sherzod-Shuxrat <sherzod.shuxrat@mail.ru>
2024-03-26 12:32:25 +0500

    Add

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        MathFunctions m = new MathFunctions();
+        System.out.println(m.abs(-3.8));
+        System.out.println(m.add(3.5,2));
+        System.out.println(m.sub(1,5));
+        System.out.println(m.div(2.22,5));
+    }
+}
---
+++ src/MathFunctions.java
@@ -0,0 +1,189 @@
+public class MathFunctions {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+    public long add(long a, long b){
+        return a+b;
+    }
+    public long add(int a, long b){
+        return a+b;
+    }
+    public long add(Long a, int b){
+        return a+b;
+    }
+    public double add(double a, double b){
+        return a+b;
+    }
+    public double add(double a, int b){
+        return a+b;
+    }
+    public double add(int a, double b){
+        return a+b;
+    }
+    public double add(double a, long b){
+        return a+b;
+    }
+    public double add(long a, double b){
+        return a+b;
+    }
+    public int sub(int a, int b){
+        return a-b;
+    }
+    public long sub(long a, long b){
+        return a-b;
+    }
+    public long sub(int a, long b){
+        return a-b;
+    }
+    public long sub(long a, int b){
+        return a-b;
+    }
+    public double sub(double a, double b){
+        return a-b;
+    }
+    public double sub(double a, int b){
+        return a-b;
+    }
+    public double sub(int a, double b){
+        return a-b;
+    }
+    public double sub(double a, long b){
+        return a-b;
+    }
+    public double sub(long a, double b){
+        return a-b;
+    }
+    public float sub(float a, float b){
+        return a-b;
+    }
+    public float sub(float a, int b){
+        return a-b;
+    }
+    public float sub(int a, float b){
+        return a-b;
+    }
+    public float sub(float a, long b){
+        return a-b;
+    }
+    public float sub(long a, float b){
+        return a-b;
+    }
+    public int multiply(int a, int b){
+        return a*b;
+    }
+    public long multiply(long a, long b){
+        return a*b;
+    }
+    public long multiply(int a, long b){
+        return a*b;
+    }
+    public long multiply(long a, int b){
+        return a*b;
+    }
+    public double multiply(double a, double b){
+        return a*b;
+    }
+    public double multiply(double a, int b){
+        return a*b;
+    }
+    public double multiply(int a, double b){
+        return a*b;
+    }
+    public double multiply(double a, long b){
+        return a*b;
+    }
+    public double multiply(long a, double b){
+        return a*b;
+    }
+    public float multiply(float a, float b){
+        return a*b;
+    }
+    public float multiply(float a, int b){
+        return a*b;
+    }
+    public float multiply(int a, float b){
+        return a*b;
+    }
+    public float multiply(float a, long b){
+        return a*b;
+    }
+    public float multiply(long a, float b){
+        return a*b;
+    }
+    public float div(int a, int b){
+        return (float) (a*1.0/b);
+    }
+    public float div(long a, long b){
+        return (float) (a*1.0/b);
+    }
+    public float div(int a, long b){
+        return (float) (a*1.0/b);
+    }
+    public float div(long a, int b){
+        return (float) (a*1.0/b);
+    }
+    public double div(double a, double b){
+        return (double) (a*1.0/b);
+    }
+    public double div(double a, int b){
+        return (double) (a*1.0/b);
+    }
+    public double div(int a, double b){
+        return (double) (a*1.0/b);
+    }
+    public double div(double a, long b){
+        return (double) (a*1.0/b);
+    }
+    public double div(long a, double b){
+        return (double) (a*1.0/b);
+    }
+    public float div(float a, float b){
+        return (float) (a*1.0/b);
+    }
+    public float div(float a, int b){
+        return (float) (a*1.0/b);
+    }
+    public float div(int a, float b){
+        return (float) (a*1.0/b);
+    }
+    public float div(float a, long b){
+        return (float) (a*1.0/b);
+    }
+    public float div(long a, float b){
+        return (float) (a*1.0/b);
+    }
+
+    public int abs(int a){
+        if (a < 0)
+            return -a;
+        return a;
+    }
+    public long abs(long a){
+        if (a < 0)
+            return -a;
+        return a;
+    }
+    public float abs(float a){
+        if (a < 0)
+            return -a;
+        return a;
+    }
+    public double abs(double a){
+        if (a < 0)
+            return -a;
+        return a;
+    }
+    public int pow(int a){
+        return a*a;
+    }
+    public long pow(long a){
+        return a*a;
+    }
+    public float pow(float a){
+        return a*a;
+    }
+    public double pow(double a){
+        return a*a;
+    }
+}
